7371ef2bf6a7b87c95c732f44de3eb5516c69400
sidrathore <rathoresiddharth0207@gmail.com>
2023-02-18 04:04:30 +0530

    otp page made

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_application_1/login_page.dart';
+import 'package:flutter_application_1/otp_page.dart';
 import 'package:flutter_application_1/splash_screen.dart';
 import 'firebase_options.dart';
 void main() {
@@ -8,6 +9,7 @@ void main() {
     home: SplashScreen(),
     routes: {
       'loginPage':(context) => LoginPage(),
+      'otpPage':(context) => OTPPage(),
     },
     
   ));
